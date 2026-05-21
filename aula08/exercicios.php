41f498b14108030a65a77fb4687cbfd8ce509259
Vitoria Moreira Pires <vitoriapquenatalento@gmail.com>
2026-05-21 12:21:44 -0300

    Add files via upload

---
+++ aula08/exercicios.php
@@ -0,0 +1,36 @@
+<?php
+
+//ex1
+     $nomes = ["Livia", "Vitoria", "Isabelly", "Issabella", "Luiza"];
+
+     foreach ($nomes as $valor) {
+        echo "$valor <br>";
+     }
+
+     echo "<hr>";
+
+//ex2
+    $nomes [] = "Priscilla";
+
+    foreach ($nomes as $valor) {
+        echo "$valor <br>";
+     }
+
+     echo "<hr>";
+
+//ex3 
+    $nomes[1] = "Vih";
+
+    foreach ($nomes as $valor) {
+        echo "$valor <br>";
+     }
+
+     echo "<hr>";
+//ex4
+    $nums = [5, 7, 10, 8, 10, 4, 10, 5, 9, 6];
+
+    foreach ($nums as $valor) {
+        echo "$valor";
+     }
+?>
+
